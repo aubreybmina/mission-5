ef9e1507f3dc7c80db28f1b2fc6ddb7b74b87784
Aubrey Mina <aubreybmina@gmail.com>
2023-05-31 03:00:37 +1200

    Improved error handling for better unit testing

--- api/src/services/laptopService.ts
+++ api/src/services/laptopService.ts
@@ -1,7 +1,7 @@
 import { ILaptop } from '../types/Interfaces'
 import Laptop from '../models/Laptop'
 import mongoose from 'mongoose'
-import Logging from '../library/Logging'
+import createHttpError from 'http-errors'
 
 export const createLaptop = async (input: ILaptop) => {
   const laptop = new Laptop({
@@ -27,64 +27,66 @@ export const createLaptop = async (input: ILaptop) => {
 }
 
 export const getLaptop = async (laptopId: string) => {
-  try {
-    const laptop = await Laptop.findById(laptopId).populate('userName')
-    return laptop
-  } catch (e) {
-    const error = new Error('No record found.')
-    Logging.error(error)
+  if (!mongoose.isValidObjectId(laptopId)) {
+    throw createHttpError(400, 'Invalid product ID.')
   }
+  const laptop = await Laptop.findById(laptopId).populate('userName')
+  if (!laptop) {
+    throw createHttpError(404, 'Product not found')
+  }
+  return laptop
 }
 
 export const getAllLaptops = async () => {
-  let laptops
-  try {
-    laptops = await Laptop.find().populate('userName')
-  } catch (e) {
-    const error = new Error('No records found.')
-    Logging.error(error)
+  const laptops = await Laptop.find().populate('userName')
+  if (laptops.length === 0) {
+    throw createHttpError(404, 'No records yet.')
   }
   return laptops
 }
 
 export const updateLaptop = async (laptopId: string, input: ILaptop) => {
-  let laptop
-  try {
-    laptop = await Laptop.findById(laptopId)
+  if (!mongoose.isValidObjectId(laptopId)) {
+    throw createHttpError(400, 'Invalid product ID.')
+  }
+  const laptop = await Laptop.findById(laptopId)
 
-    if (laptop) {
-      const newInput = {
-        brand: input.brand,
-        model: input.model,
-        category: input.category,
-        screenSize: input.screenSize,
-        screenResolution: input.screenResolution,
-        cpuFamily: input.cpuFamily,
-        memorySize: input.memorySize,
-        ssdCapacity: input.ssdCapacity,
-        price: input.price,
-        operatingSystem: input.operatingSystem,
-        warranty: input.warranty,
-        features: input.features,
-        userName: input.userName,
-      }
-      laptop.set(newInput)
-      laptop.save()
+  if (!laptop) {
+    throw createHttpError(404, 'Product not found')
+  }
 
-      return laptop
+  if (laptop) {
+    const newInput = {
+      brand: input.brand,
+      model: input.model,
+      category: input.category,
+      screenSize: input.screenSize,
+      screenResolution: input.screenResolution,
+      cpuFamily: input.cpuFamily,
+      memorySize: input.memorySize,
+      ssdCapacity: input.ssdCapacity,
+      price: input.price,
+      operatingSystem: input.operatingSystem,
+      warranty: input.warranty,
+      features: input.features,
+      userName: input.userName,
     }
-  } catch (e) {
-    const error = new Error('No record found.')
-    Logging.error(error)
+    laptop.set(newInput)
+    laptop.save()
+
+    return laptop
   }
 }
 
 export const deleteLaptop = async (laptopId: string) => {
-  try {
-    const laptop = await Laptop.findByIdAndDelete(laptopId)
-    return `${laptop?.brand} ${laptop?.model} has been deleted.`
-  } catch (e) {
-    const error = new Error('No record found.')
-    Logging.error(error)
+  if (!mongoose.isValidObjectId(laptopId)) {
+    throw createHttpError(400, 'Invalid product ID.')
   }
+  const laptop = await Laptop.findByIdAndDelete(laptopId)
+
+  if (!laptop) {
+    throw createHttpError(404, 'Product not found')
+  }
+
+  return `${laptop?.brand} ${laptop?.model} has been deleted.`
 }
