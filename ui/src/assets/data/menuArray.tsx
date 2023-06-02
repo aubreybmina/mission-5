daf5649e83a1caabf9b0a3d385122501483e1e11
WDuncker <wilhelmusd@missionreadyhq.com>
2023-06-03 09:02:17 +1200

    ui updates

---
+++ ui/src/assets/data/menuArray.tsx
@@ -0,0 +1,93 @@
+import MacbookLineIcon from 'remixicon-react/MacbookLineIcon'
+import ComputerLineIcon from 'remixicon-react/ComputerLineIcon'
+import KeyboardBoxLineIcon from 'remixicon-react/KeyboardBoxLineIcon'
+import WifiLineIcon from 'remixicon-react/WifiLineIcon'
+import PrinterLineIcon from 'remixicon-react/PrinterLineIcon'
+import Tv2LineIcon from 'remixicon-react/Tv2LineIcon'
+import SmartphoneLineIcon from 'remixicon-react/SmartphoneLineIcon'
+import CustomerServiceLineIcon from 'remixicon-react/CustomerServiceLineIcon'
+import GamepadFillIcon from 'remixicon-react/GamepadFillIcon'
+import CameraFillIcon from 'remixicon-react/CameraFillIcon'
+import KeyFillIcon from 'remixicon-react/KeyFillIcon'
+import RobotFillIcon from 'remixicon-react/RobotFillIcon'
+import AddLineIcon from 'remixicon-react/AddLineIcon'
+import GiftLineIcon from 'remixicon-react/GiftLineIcon'
+import AppleFillIcon from 'remixicon-react/AppleFillIcon'
+
+export const menuArray = [
+  {
+    id: 1,
+    icon: MacbookLineIcon,
+    option: 'Computers & Tablets',
+  },
+  {
+    id: 2,
+    icon: ComputerLineIcon,
+    option: 'PC Peripherals & Accesories',
+  },
+  {
+    id: 3,
+    icon: KeyboardBoxLineIcon,
+    option: 'PC Parts',
+  },
+  {
+    id: 4,
+    icon: WifiLineIcon,
+    option: 'Networking',
+  },
+  {
+    id: 5,
+    icon: PrinterLineIcon,
+    option: 'Printing & Office',
+  },
+  {
+    id: 6,
+    icon: SmartphoneLineIcon,
+    option: 'Phones & Accesories',
+  },
+  {
+    id: 7,
+    icon: Tv2LineIcon,
+    option: 'TV & AV',
+  },
+  {
+    id: 8,
+    icon: CustomerServiceLineIcon,
+    option: 'Headphones & Audio',
+  },
+  {
+    id: 9,
+    icon: GamepadFillIcon,
+    option: 'Gaming',
+  },
+  {
+    id: 10,
+    icon: CameraFillIcon,
+    option: 'Cameras & Drones',
+  },
+  {
+    id: 11,
+    icon: KeyFillIcon,
+    option: 'Smart Home & Security',
+  },
+  {
+    id: 12,
+    icon: RobotFillIcon,
+    option: 'Toys, Hobbies & STEM',
+  },
+  {
+    id: 13,
+    icon: AddLineIcon,
+    option: 'More',
+  },
+  {
+    id: 14,
+    icon: GiftLineIcon,
+    option: 'Gift Ideas',
+  },
+  {
+    id: 15,
+    icon: AppleFillIcon,
+    option: 'Apple',
+  },
+]
